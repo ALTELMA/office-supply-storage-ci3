53cb47b32c01a174a30ccc83444b8471fb101ec1
unknown <nrohtgnohp@gmail.com>
2016-05-04 02:39:25 +0700

    Delete unused files

--- application/libraries/PHPExcel/branches/v1.5.5/setpath.bat
+++
@@ -1,5 +0,0 @@
-@set PHPINSTALLDIR=C:\php5;C:\LAMP\php;C:\LAMP\php5;D:\LAMP\php5
-@set PATH=%PHPINSTALLDIR%;%PATH%;
-
-cd Tests
-cls
--- application/libraries/PHPExcel/branches/v1.6.3/setpath.bat
+++
@@ -1,5 +0,0 @@
-@set PHPINSTALLDIR=C:\PHP\5.2.5;C:\php5;C:\LAMP\php;C:\LAMP\php5;D:\LAMP\php5
-@set PATH=%PHPINSTALLDIR%;%PATH%;
-
-cd Tests
-cls
